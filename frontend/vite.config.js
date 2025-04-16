c77fe6737bccfce5555ad4c5d6b31f4480e9cb06
hijera <hi-jera@yandex.ru>
2025-04-16 16:21:02 +0300

    update vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,5 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: '/course_site_test/', // Имя репозитория GitHub
+  base: '/course_site/', // Имя репозитория GitHub
 })
